c9feca0d89a6670b9631001c86ec6c296a601977
Hazem Mohamed <hazemshata200@gmail.com>
2023-12-10 23:37:33 +0200

    Added MongoDB to the project

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
 import express from "express"
+import mongoose from "mongoose"
+import * as dotenv from "dotenv"
+dotenv.config({ path: ".env" });
+
 import toDoRoutes  from "./routes/toDoRoutes"
 import userRoutes from "./routes/userRoutes"
 
 
-const port = 3000
-const app = express()   
+const app = express()
+
+const port = process.env.PORT
+const dataBaseConnectionSTR = process.env.DATABASE_CONNECTION_STRING
+
+
 app.use(express.json())
 
 
@@ -12,6 +20,7 @@ app.use('/todo', toDoRoutes)
 app.use('/users', userRoutes)
 
 
-app.listen(port, ()=>{
-    console.log(`Alive on http://localhost:${port}/`)
-})
+mongoose.connect(dataBaseConnectionSTR!).then(()=>{
+    console.log('Connected to database')
+    app.listen(port, ()=>{console.log(`Alive on http://localhost:${port}/`)})
+}).catch((err)=>{console.log(`Error: ${err}`)})
